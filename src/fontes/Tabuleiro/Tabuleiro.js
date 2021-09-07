a31f4172ac986cbb3e2b6a66b4570c8976009faf
Fernando Abdalla <fernandoaoborges@gmail.com>
2021-09-07 10:09:13 -0300

    JOGO - movimentação pronta/Marcação de Celulas

--- src/fontes/Tabuleiro/Tabuleiro.js
+++ src/fontes/Tabuleiro/Tabuleiro.js
@@ -1,16 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./Tabuleiro.css";
 
 const Tabuleiro = () => {
-  const [posicaoClicada, setPosicaoClicada] = useState(null);
+  const [elementoClicado, setElementoClicado] = useState(null);
+  const [tabuleiro, setTabuleiro] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [jogadores, setJogadores] = useState(0);
+  const [marcacaoCelulas, setMarcacaoCelulas] = useState(["x", "o"]);
+  const [fimJogo, setFimJogo] = useState(false);
 
-  console.log(posicaoClicada);
+  // console.log(tabuleiro);
+
+  const moverPecas = useCallback(
+    (elemento) => {
+      // setElementoClicado(null);
+      if (elemento) {
+        if (fimJogo) return;
+
+        const arraySuporte = [...tabuleiro];
+        setElementoClicado(elemento);
+        const { id } = elemento.target;
+
+        arraySuporte[id] = marcacaoCelulas[jogadores];
+        jogadores === 0 ? setJogadores(1) : setJogadores(0);
+        setTabuleiro(arraySuporte);
+      }
+    },
+    [jogadores, marcacaoCelulas, tabuleiro, fimJogo]
+  );
+
+  const marcaCelulas = useCallback(() => {
+    if (elementoClicado) {
+      const { id, innerHTML } = elementoClicado.target;
+
+      let MarcacaoTabuleiro = tabuleiro[id];
+
+      elementoClicado.target.innerHTML = `<h1>${MarcacaoTabuleiro}</h1>`;
+    }
+  }, [elementoClicado, tabuleiro]);
+
+  useEffect(() => {
+    marcaCelulas();
+  }, [marcaCelulas]);
+
+  // console.log(marcacaoCelulas);
 
   const retornaCelula = (index) => (
     <div
       id={index}
       className="celulas"
-      onClick={(event) => setPosicaoClicada(event.target.id)}
+      onClick={(event) => moverPecas(event)}
     ></div>
   );
 
